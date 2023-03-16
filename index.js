901b1b285f6ba404f0811d552e48e262cee7799c
Patricia <wambuipatricia3@gmail.com>
2023-03-16 13:35:51 +0300

    Completed Assignments

--- index.js
+++ index.js
@@ -1,33 +1,31 @@
+
 const cats = ["Milo", "Otis", "Garfield"];
 
 function destructivelyAppendCat(name) {
-    cats.push(name);
+cats.push("Ralph");
 }
 
 function destructivelyPrependCat(name) {
-cats.unshift(name);
+cats.unshift("Bob");
 }
 
 function destructivelyRemoveLastCat(name) {
-    cats.pop(name);
+cats.pop(name);
 }
 
 function destructivelyRemoveFirstCat(name) {
-    cats.shift(name);
+cats.shift(name);
 }
 function appendCat(name) {
-    return [...cats, name];
-  }
-  
-  function prependCat(name) {
-    return [name, ...cats];
-  }
-  
-  function removeLastCat() {
-    return cats.slice(0, -1);
-  }
-  
-  function removeFirstCat() {
-    return cats.slice(1);
-  }
-  
+return [...cats, "Broom"];
+}
+function prependCat(name) {
+return ["Arnold", ...cats];
+}
+function removeLastCat() {
+return cats.slice(0, cats.length-1);
+}
+function removeFirstCat() {
+return cats.slice(1);
+}
+
